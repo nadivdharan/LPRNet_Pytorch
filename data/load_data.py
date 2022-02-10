b558d11585c53a46be913e533f30557b97e62fc9
Nadiv Dharan <nadivd@hailo.ai>
2022-02-10 12:08:47 +0000

    include aug

--- data/load_data.py
+++ data/load_data.py
@@ -4,7 +4,9 @@ from imutils import paths
 import numpy as np
 import random
 import cv2
+from PIL import Image
 import os
+import imgaug.augmenters as iaa
 
 CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
@@ -14,8 +16,65 @@ CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 
 CHARS_DICT = {char:i for i, char in enumerate(CHARS)}
 
+
+def image_aug(img):
+    seq = iaa.Sequential([
+        iaa.SomeOf((0, 5),[
+            iaa.Crop(percent=(0, 0.05)),
+            iaa.OneOf([  # add blur
+                iaa.GaussianBlur((0, 3.0)),
+                iaa.AverageBlur(k=(2, 7)),
+                iaa.MedianBlur(k=(3, 11)),
+            ]),
+            iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen each image
+            iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
+            iaa.LinearContrast((0.75, 1.5)),  # improve/destroy image contrast
+            iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.03*255), per_channel=0.2),  # add noise
+            iaa.Multiply((0.5, 0.7), per_channel=0.2),  # change brightness
+            iaa.Grayscale(alpha=(0.0, 1.0)),  # remove/strenghts colors
+            iaa.OneOf([
+                iaa.Dropout((0.005, 0.01), per_channel=0.015),
+                iaa.CoarseDropout(
+                    (0.005, 0.01), size_percent=(0.005, 0.01),
+                    per_channel=0.015
+                ),
+            ]),
+            iaa.ElasticTransformation(alpha=(0.5, 1.5), sigma=0.9),  # move pixels
+            iaa.Dropout((0.01, 0.05), per_channel=0.9),
+            iaa.Add((-10, 10), per_channel=0.5),
+            iaa.PiecewiseAffine(scale=(0.01, 0.05)),  # distort local areas
+            iaa.Invert(0.05, per_channel=True),
+            iaa.Affine(
+                scale={"x": (0.99, 1.01), "y": (0.99, 1.01)},
+                translate_percent={"x": (-0.03, 0.03), "y": (-0.03, 0.03)},
+                rotate=(-10, 10),
+                shear=(-10, 10),
+                order=[0, 1],
+                cval=(0, 255)
+            )
+        ], random_order=True)
+    ], random_order=True)
+    return np.squeeze(seq(images=np.expand_dims(np.array(img, np.uint8), axis=0)))
+
+
+def pixelize(img, width, height, factors=(4, 5), p=0.5):
+    factor = random.choice(factors)
+    w = int(width/factor)
+    h = int(height/factor)
+    interp = Image.NEAREST if random.random() >= p else Image.BILINEAR
+    # img2save = Image.fromarray(img)
+    # img2save.save(os.path.join(os.getcwd(), 'before.png'))
+    img = np.expand_dims(np.array(Image.fromarray(np.squeeze(img)).resize((w, h))), axis=0)
+    img = np.array(Image.fromarray(np.squeeze(img)).resize((width, height), resample=interp))
+    # img2save = Image.fromarray(img)
+    # img2save.save(os.path.join(os.getcwd(), 'after.png'))
+    return img
+
+
 class LPRDataLoader(Dataset):
-    def __init__(self, img_dir, imgSize, lpr_max_len, PreprocFun=None):
+    def __init__(self, img_dir, imgSize, lpr_max_len, PreprocFun=None, train=True, crop=None):
+        self.crop = crop
+        self.train = train
         self.img_dir = img_dir
         self.img_paths = []
         for i in range(len(img_dir)):
@@ -34,6 +93,10 @@ class LPRDataLoader(Dataset):
     def __getitem__(self, index):
         filename = self.img_paths[index]
         Image = cv2.imread(filename)  # BGR
+        if len(Image.shape) == 2:
+            Image = np.repeat(np.expand_dims(Image, axis=-1), 3, axis=-1)
+        if self.crop is not None:
+            Image = Image[:, self.crop:, :]
         height, width, _ = Image.shape
         if height != self.img_size[1] or width != self.img_size[0]:
             Image = cv2.resize(Image, self.img_size)
@@ -57,6 +120,10 @@ class LPRDataLoader(Dataset):
         return Image, label, len(label)
 
     def transform(self, img):
+        if self.train:
+            img = image_aug(img)
+            if random.random() >= 0.5:
+                img = pixelize(img, width=self.img_size[0], height=self.img_size[1])
         img = img.astype('float32')
         img -= 127.5
         img *= 0.0078125 # * (1/128)
